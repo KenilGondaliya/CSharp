b7de17d6522aa3a8e55940d59f9cfed4715b9426
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2026-01-23 19:24:56 +0530

    commit

---
+++ MyApp/ObjectDemo/ObjectDemo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApp.ObjectDemo
+{
+    public class ObjectDemo
+    {
+        public static void Run()
+        {
+            // every class in C# directly or indirectly inherits Object class
+            Object obj = new Object();
+        }
+    }
+}
--- MyApp/obj/Debug/net10.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net10.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ba39e939197f896494bda7502441ddb393ebf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05f575474585b84a103fdb7b928ec39ed0258e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
